0d0fb3087bee7df82f51ee1b0f03acc6b420f529
Oguzhan YUKSEKTEPE <106281089+Oguzhanytp@users.noreply.github.com>
2022-06-05 18:42:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,61 @@
-print("********************\nATM SİSTEMİNE HOŞGELDİNİZ\n********************")
-
 print("""
-İŞLEMLER:
-1. Bakiye Sorgulama
-2. Para Yatırma
-3. Para Çekme
 
-Çıkış için '0'tuşuna basınız.
+**********/nKÜTÜPHANEMİZE HOŞGELDİNİZ/n**********
+
 """)
 
-bakiye = 1000
+roman =  { 'kırlangıç çığlığı' : {
+    'Yazar' : 'Ahmet Umit',
+    'Basim_Yili' : '2005',
+    'Raf' : 'A1'
+},
+    'hayalet' : {
+        'Yazar' : 'John Black',
+        'Basim Yili' : '1995',
+        'Raf' : 'A2'
+    }
+}
+
+print("""
+1.) Kitap almak istiyorum = 1
+2.) Kitap eklemek istiyorum = 2
+3.) Kitap listesini görmek istiyorum = 3
+""")
 
 while True:
-    islem = int(input('Lütfen işlem seçiniz : '))
+    islem = input("Yapmak istediğiniz işlemi seçin = ")
 
-    if (islem == 0):
-        print('Güle Güle :)')
-        break
-    elif (islem == 1):
-         print(f"Bakiyeniz {bakiye} TL")
-    elif (islem == 2):
-        yatirilacak_miktar = int(input("Yatırmak istediğiniz tutarı girin :"))
-        bakiye += yatirilacak_miktar
-    elif (islem == 3):
-        cekilecek_miktar = int(input('Çekmek istediğiniz tutarı girin : '))
-        if (bakiye < cekilecek_miktar):
-            print("BAKİYENİZ YETERSİZ.")
-            print(f"Bakiyeniz {bakiye} TL")
-            continue
-        bakiye -= cekilecek_miktar
-        print(f"Kalan miktar {bakiye} TL")
-    else:
-        print("LÜTFEN GEÇERLİ BİR İŞLEM GİRİN!")
+    if (islem == '1'):
+        kitapCik = input('Almak istediğiniz kitap ismini girin : ')
+        if (kitapCik not in roman):
+            print('Bu kitap veritabanınızda yoktur.')
+        else:
+            KucukKitapCik = kitapCik.islower()
+            alinanKitap = roman[kitapCik]
+            print('----------Almak istediğiniz kitap bilgileri----------')
+            for i in alinanKitap:
+                print(i + ' : ' + alinanKitap[i])
+            print('Almak istiyorsanız (e) vazgeçmek istiyoranız (h) basın:')
+            karar = str(input('İşlem seçin : '))
+            cikildi  = roman.pop(kitapCik)
+            print("-----KÜTÜPHANEDE KALAN KİTAPLAR-----")
+        print(f"-----Kütüphanede olan kitaplar-----\n{roman}")
+    elif (islem == '2'):
+        kitapEkle = input("Kitap ismini girin : ")
+        KucukKitapEkle = kitapEkle.islower()
+        yazarismi = input("Yazar ismi giriniz :  ")
+        YazarEkleKucuk = yazarismi.islower()
+        basimYili = input("Basım yılı girin : ")
+        raf = input("Koymak istediğiniz rafı girin : ")
+        RafEkleBuyuk = raf.isupper()
 
+        ekle = roman[kitapEkle] = {
+            'Yazar İsmi' : yazarismi,
+            'Basım Yılı' : basimYili,
+            'raf ' : raf
+        }
+    elif (islem == '3'):
+        print("""
+        ---------- KÜTÜPHANEDEKİ TÜM KİTAPLAR ----------
+        """)
+        print(roman)
